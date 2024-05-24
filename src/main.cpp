8452aeb7cfc60991c6ab40f069d26104123ef7ab
MatzLeeflang <matz@leeflang-it.nl>
2024-05-24 13:03:58 +0200

    made LoRa receiver class and set it in the main

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,20 @@
 #include <Arduino.h> 
 #include "pico/stdlib.h"
 #include <trilaterate.hpp>
+#include <LoRaReceiver.h>
 
 
 int main(void)
 {
+    stdio_init_all();
 
+    LoRaReceiver lora;
+    lora.start();
+
+    while (1)
+    {
+        lora.read();
+    }
+    
     return 0;
 }
