f646d4412da422088d6590d902accae57fa6b2a6
jkl46 <jay-klein@outlook.com>
2024-05-27 14:20:20 +0200

    comments to main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,12 @@
+// Do not change this file
+// Work in master/master.cpp and slave/slave.cpp as if this that the main file
+
+/*----------------CHANGE COMMENT BELOW------------------*/
+
 #define MASTER
 // #define SLAVE
+
+/*----------------DO NOT CHANGE BELOW-------------------*/
 
 #ifdef MASTER
 #define master_main build_main
