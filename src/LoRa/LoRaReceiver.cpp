2f0458e83c3af5484ac7718561716c0e11ee658f
MatzLeeflang <matz@leeflang-it.nl>
2024-05-28 10:04:11 +0200

    changed the Serial.print to printf cause it didnt work with Serial.print

--- src/LoRa/LoRaReceiver.cpp
+++ src/LoRa/LoRaReceiver.cpp
@@ -6,23 +6,22 @@
 
 void LoRaReceiver::start(){
     if (!LoRa.begin(500E6)) {
-      Serial.println("Starting LoRa failed!\n");
+      printf("Starting LoRa failed!\n");
    }
 
-    Serial.println("LoRa Started\n");   
+    printf("LoRa Started\n");   
 }
 
 void LoRaReceiver::read(){
       int packetSize = LoRa.parsePacket();
       if (packetSize) {
          // received a packet
-         Serial.println("Received packet \n'");
+         printf("Received packet \n'");
 
          // read packet
          while (LoRa.available()) {
             char i = LoRa.read();
-            //for now print to serial, this will change once we are able to save it to the flash
-            Serial.println(i);
+            printf("%c", i);
          }
     }
 }
