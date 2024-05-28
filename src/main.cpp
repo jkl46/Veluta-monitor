adb1fe944e29f6ef1e3145e61242d585e4d388c3
MatzLeeflang <matz@leeflang-it.nl>
2024-05-28 10:19:44 +0200

    main changed

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,13 @@
 /*----------------DO NOT CHANGE BELOW-------------------*/
 
 #ifdef MASTER
-#define master_main build_main
-#include <master/master.cpp>
+#warning "BUILDING MASTER!"
+extern int master_main(int argc, char** argv);
 #endif
 
 #ifdef SLAVE
-#define slave_main build_main
-#include <slave/slave.cpp>
+#warning "BUILDING SLAVE!"
+extern int slave_main(int argc, char** argv);
 #endif
 
 #include "pico/stdlib.h"
@@ -23,5 +23,13 @@
 int main(int argc, char** argv)
 {
     stdio_init_all();
-    return build_main(argc, argv);
+
+    /*__________Run master or slave main________*/
+    #ifdef MASTER
+    return master_main(argc, argv);
+    #endif
+
+    #ifdef SLAVE
+    return slave_main(argc, argv);
+    #endif
 }
