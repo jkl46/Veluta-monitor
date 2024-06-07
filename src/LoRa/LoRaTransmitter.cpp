1468c22bd377dee7ee29da67a38cc212f6289844
MatzLeeflang <matz@leeflang-it.nl>
2024-06-07 15:13:43 +0200

    and again(i know i shouldve checked them all the first time)

--- src/LoRa/LoRaTransmitter.cpp
+++ src/LoRa/LoRaTransmitter.cpp
@@ -12,6 +12,6 @@ void LoRaTransmitter::start()
 void LoRaTransmitter::send(lora_data* data)
 {
     LoRa.beginPacket();
-    LoRa.print("**SLAVE_ID: #" + std::to_string(data->id) + "@;gps: latitude #"+ std::to_string(data->latitude) +"@;longitude #" + std::to_string(data->longitude) + "@;AreaCode: #" + std::to_string(data->area_code) + "@;Hornet_id: #" + std::to_string(data->hornet_id) + "@");	
+    LoRa.print("**SLAVE_ID: #" + std::to_string(data->monitor_id) + "@;gps: latitude #"+ std::to_string(data->latitude) +"@;longitude #" + std::to_string(data->longitude) + "@;AreaCode: #" + std::to_string(data->area_code) + "@;Hornet_id: #" + std::to_string(data->hornet_id) + "@");	
     LoRa.endPacket();
 }
