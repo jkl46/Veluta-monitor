73af44c43314c819da2d8a0990144c2fc4ac4d9c
jkl46 <jay-klein@outlook.com>
2024-06-10 13:01:34 +0200

    change function lon_degree_distance and lat_degree_distance to defines

--- src/trilaterate.cpp
+++ src/trilaterate.cpp
@@ -7,6 +7,14 @@
 #include <math.h>
 #include "trilaterate.hpp"
 
+// Calculate radian from degree
+#define DEGREE_TO_RAD(degree) (degree * M_PI / 180.0)
+
+// Formula for length of degree longitude at latitude 'lat'. Reference: https://en.wikipedia.org/wiki/Geographic_coordinate_system
+#define lon_degree_distance(lat) (111412.84 * cos(DEGREE_TO_RAD(lat))) - (93.5 * cos(3 * DEGREE_TO_RAD(lat))) + (0.118 * cos(5 * DEGREE_TO_RAD(lat)))
+// Formula for length of degree latitude at latitude 'lat'. Reference: https://en.wikipedia.org/wiki/Geographic_coordinate_system
+#define lat_degree_distance(lat) (111132.92 - 559.82 * cos(2 * DEGREE_TO_RAD(lat))) + (1.175 * cos(4 * DEGREE_TO_RAD(lat))) + (0.0023 * cos(6 * DEGREE_TO_RAD(lat)))
+
 // Reference: https://paulbourke.net/geometry/circlesphere/
 int circle_circle_intersection(circle_t circle1, circle_t circle2, point_t *intersection1, point_t *intersection2)
 {
@@ -102,21 +110,23 @@ int line_line_intersect(line_t line1, line_t line2, point_t *result)
 
 // Calculate length of degree of longitude at latitude x in meters
 // Reference: https://en.wikipedia.org/wiki/Geographic_coordinate_system
-double lon_degree_distance(double lon)
-{
-  // Degrees to radians
-  lon = lon * M_PI / 180.0;
-  return (111412.84 * cos(lon)) - (93.5 * cos(3 * lon)) + (0.118 * cos(5 * lon));
-}
+/* Function has been change to define*/
+// double lon_degree_distance(double lon)
+// {
+//   // Degrees to radians
+//   lon = lon * M_PI / 180.0;
+//   return (111412.84 * cos(lon)) - (93.5 * cos(3 * lon)) + (0.118 * cos(5 * lon));
+// }
 
 // Calculate length of degree of latitude at latitude x in meters
 // Reference: https://en.wikipedia.org/wiki/Geographic_coordinate_system
-double lat_degree_distance(double lat)
-{
-	// Degrees to radians
-	lat = lat * M_PI / 180.0;
-	return (111132.92 - 559.82 * cos(2 * lat)) + (1.175 * cos(4 * lat)) + (0.0023 * cos(6 * lat));
-}
+/* Function has been change to define*/
+// double lat_degree_distance(double lat)
+// {
+// 	// Degrees to radians
+// 	lat = lat * M_PI / 180.0;
+// 	return (111132.92 - 559.82 * cos(2 * lat)) + (1.175 * cos(4 * lat)) + (0.0023 * cos(6 * lat));
+// }
 
 int trilaterate(record_t record1, record_t record2, record_t record3, coord_t *trilaterationCoord, coord_t *intersectionCoord)
 {
