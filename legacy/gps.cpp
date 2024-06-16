c060f3afe222a7530b5126bae0dffc73989aece2
jkl46 <jay-klein@outlook.com>
2024-06-16 20:17:48 +0200

    merge from pull and refactor concering gps module

--- legacy/gps.cpp
+++ legacy/gps.cpp
@@ -1,4 +1,3 @@
-
 #include "gps.hpp"
 #define UART_data_bits 8
 #define UART_stop_bits 1
@@ -21,25 +20,80 @@ void init_UART(uart_inst_t *UART_ID, uint UART_TX_PIN, uint UART_RX_PIN, uint BA
     uart_set_format(UART_ID, UART_data_bits, UART_stop_bits, UART_PARITY_NONE); 
 }
 
-char *read_UART(uart_inst_t *UART_ID)
+std::vector<char> read_UART(uart_inst_t *UART_ID)
 {
     /*
-    const u_int8_t = Maximum size of the array of received data
-    static char = Array to store received data
-    return gps_data = Return received data
-
-    This function reads data from the UART,
-    and stores it in an array and returns the array
-
+    this function reads the UART data and stores it in a vector
+    the function reads the UART data until it reaches the stop character #
+    the function gps_data.push_back(ch) puts the character in the vector
     */
-    const u_int8_t array_size = 255; 
-    static char gps_data[array_size]; 
+    const char stop_char = '#';
+    std::vector<char> gps_data;
     int index = 0;
-    while (uart_is_readable(UART_ID) && index < array_size)
+    while (uart_is_readable(UART_ID) && gps_data[index-1] != stop_char)
     {
         char ch = uart_getc(UART_ID);
-        gps_data[index++] = ch;
+        gps_data.push_back(ch);
+        index++;
     }
-    gps_data[index] = '\0';
-    return gps_data; 
+    return gps_data;
+}
+
+Coordinates gps::send_gps(std::vector<char> gps_data) {
+    /*
+    this function first converts the vector to an array
+    it puts a null character at the end of the array to make sure 
+    the program knows where the array ends, and that it wil not crash.
+    then it tokenizes the array to extract the latitude and longitude
+
+    it does this by using strtok to tokenize the array
+    this function works by counting the number of commas 
+    in the array and divides the array into tokens(segments).
+
+    if the count is 2, the token is the latitude
+    if the count is 4, the token is the longitude
+
+    the function then converts the tokens to double and stores them in a struct Coordinates
+    */
+    char gps_array[MAX_ARRAY_SIZE];
+    Coordinates coordinates;
+    coordinates.longitude = 0.0;
+    coordinates.latitude = 0.0;
+
+    for (size_t i = 0; i < gps_data.size(); i++) {
+        gps_array[i] = gps_data[i];
+    }
+
+    gps_array[gps_data.size()] = '\0';
+
+    char *token = strtok(gps_array, ",");
+    int count = 0;
+
+    while (token != NULL) {
+        if (count == 2) {
+            coordinates.latitude = atof(token);
+        } else if (count == 4) {
+            coordinates.longitude = atof(token);
+        }
+        token = strtok(NULL, ",");
+        count++;
+    }
+
+    this->latitude = coordinates.latitude;
+    this->longitude = coordinates.longitude; 
+
+    return coordinates;
+}
+
+/*
+these functions return the latitude and longitude of the gps.
+this is for easy acces to the latitude and longitude of the gps
+*/
+
+double gps::recieve_lon(){
+    return this->longitude;
+}
+
+double gps::recieve_lat(){
+    return this->latitude;
 }
--- legacy/gps.hpp
+++ legacy/gps.hpp
@@ -1,14 +1,54 @@
 #ifndef GPS_H
 #define GPS_H
 
+/*
+these includes are used to include the necessary libraries
+used for the GPS class
+*/
 #include <stdint.h>
 #include "hardware/uart.h"
 #include "hardware/gpio.h"
 #include <stdio.h>
 #include <string.h>
 #include <pico/stdlib.h>
+#include <stdbool.h>
+#include <vector>
+#include <cstdlib>
+#include <string>
 
-void init_UART(uart_inst_t *UART_ID, uint UART_TX_PIN, uint UART_RX_PIN, uint BAUDRATE);
-char *read_UART(uart_inst_t *UART_ID);
+struct Coordinates {
+    double latitude;
+    double longitude;;
+};
+
+class gps {
+    public:
+
+    // Constructor
+        gps() : latitude(0.0), longitude(0.0) {}
+    //Functions
+        void init_gps();
+        std::vector<char> read_gps();
+        Coordinates send_gps(std::vector<char> gps_data);
+        double recieve_lon();
+        double recieve_lat();
+    //Variables
+        double latitude;
+        double longitude;
+
+
+    private:
+    /*
+    these are the variables that are used to configure the UART
+    */
+        const int UART_data_bits = 8;
+        const int UART_stop_bits = 1;
+        const int UART_TX_PIN = 5;
+        const int UART_RX_PIN = 6;
+        const int BAUDRATE = 9600;
+        const int MAX_ARRAY_SIZE = 255;
+};
+
+
 
 #endif 
